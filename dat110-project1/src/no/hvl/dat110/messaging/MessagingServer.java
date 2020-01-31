54697a9330b4262a510c153f2654fb6640bb2812
h577860 <577860@stud.hvl.no>
2020-01-31 17:31:04 +0100

    Corrected merge, keeping only the most recent version.

--- dat110-project1/src/no/hvl/dat110/messaging/MessagingServer.java
+++ dat110-project1/src/no/hvl/dat110/messaging/MessagingServer.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package no.hvl.dat110.messaging;
 
 import java.io.IOException;
@@ -63,69 +62,3 @@ public class MessagingServer {
 	}
 
 }
-=======
-package no.hvl.dat110.messaging;
-
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
-import no.hvl.dat110.TODO;
-
-public class MessagingServer {
-
-	private ServerSocket welcomeSocket;
-
-	public MessagingServer(int port) {
-
-		try {
-
-			this.welcomeSocket = new ServerSocket(port);
-
-		} catch (IOException ex) {
-
-			System.out.println("Messaging server: " + ex.getMessage());
-			ex.printStackTrace();
-		}
-	}
-
-	// accept an incoming connection from a client
-	public Connection accept() {
-		
-		Socket socket = null;
-		Connection connection = null;
-
-		// TODO
-		// accept TCP connection on welcome socket and create messaging connection
-		
-		try {
-			socket = welcomeSocket.accept();
-			connection = new Connection(socket);
-		}catch(IOException ex) {
-			ex.printStackTrace();
-		}
-
-//		if (true) {
-//			throw new UnsupportedOperationException(TODO.method());
-//		}
-
-		return connection;
-
-	}
-
-	public void stop() {
-
-		if (welcomeSocket != null) {
-
-			try {
-				welcomeSocket.close();
-			} catch (IOException ex) {
-
-				System.out.println("Messaging server: " + ex.getMessage());
-				ex.printStackTrace();
-			}
-		}
-	}
-
-}
->>>>>>> branch 'master' of https://github.com/h577860/dat110_Prosjekt1.git
